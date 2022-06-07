99dbb1b0e37f51fa6dcae8a3d8f8fee313835a44
Anders Henja <anders@baltrad.eu>
2022-06-07 13:07:22 +0200

    Fix

---
+++ R/package.R
@@ -0,0 +1,13 @@
+globalVariables(c("..", "self", "private", "N"))
+
+.generator_null <- NULL
+.compilation_unit <- NULL
+
+.onAttach <- function(libname, pkgname) {
+}
+
+.onLoad <- function(libname, pkgname) {
+}
+
+.onUnload <- function(libpath) {
+}
